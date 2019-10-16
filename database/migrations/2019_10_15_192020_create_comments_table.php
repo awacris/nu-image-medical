7548154eacf35d782876fbe2f8603298c86eb437
awacris <56620848+awacris@users.noreply.github.com>
2019-10-16 01:55:21 -0400

    Update 2019_10_15_192020_create_comments_table.php

--- database/migrations/2019_10_15_192020_create_comments_table.php
+++ database/migrations/2019_10_15_192020_create_comments_table.php
@@ -16,7 +16,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('phone_no');
             $table->text('comment');
             $table->timestamps();
